c44652ed94a1fb496996e8074fbfa54997343b5f
Messi-Q <messi.qp711@gmail.com>
2021-06-26 15:46:26 +0800

    Delete produce_bytecode.sh

--- produce_bytecode.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh -
-
-path=./transorder/sourcecode/
-files=$(ls $path)
-
-for filename in $files
-do
-   solc --bin transorder/sourcecode/$filename | tee -a transorder/bytecode/$filename
-done
